deb69503344c02124d9ce92fe9404a6f628ea33b
puchiflito <aguskainer1998@outlook.com>
2023-12-23 23:38:08 -0300

    server funcionando, modelos y relaciones

---
+++ server/src/models/Activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("Activity", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      increment: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      value: {
+        min: 1,
+        max: 5,
+      },
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.ENUM("autum", "winter", "spring", "summer"),
+      allowNull: false,
+    },
+  });
+};
---
+++ server/src/models/Country.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Country", {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continents: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+};
